581e7dcd02f4e671dc33b3d520af7aaa82f66733
unknown <cdfb94471126@gmail.com>
2025-02-17 16:08:21 -0300

    fix bugs

--- main.js
+++ main.js
@@ -4,8 +4,13 @@ class MainScene extends Phaser.Scene {
     }
 
     preload() {
-        this.load.image('ship', 'obj/nave.png'); // Usa a imagem enviada
-        this.load.image('bullet', 'obj/bala.png'); // Você pode substituir por outra imagem de tiro
+        this.load.image('ship', 'obj/nave.png'); // Verifique se o caminho está correto
+        this.load.image('bullet', 'obj/bala.png'); // Verifique se o caminho está correto
+        this.load.image('upButton', 'obj/up.png'); // Verifique se o caminho está correto
+        this.load.image('leftButton', 'obj/left.png'); // Verifique se o caminho está correto
+        this.load.image('downButton', 'obj/down.png'); // Verifique se o caminho está correto
+        this.load.image('rightButton', 'obj/right.png'); // Verifique se o caminho está correto
+        this.load.image('shootButton', 'obj/shoot.png'); // Verifique se o caminho está correto
     }
 
     create() {
@@ -41,28 +46,82 @@ class MainScene extends Phaser.Scene {
                 }
             });
         }, this);
+
+        // Adicione botões de controle
+        this.addButton(100, 480, 'upButton', () => this.moveShip('up')).setScale(0.1);
+        this.addButton(50, 533, 'leftButton', () => this.moveShip('left')).setScale(0.1).setAngle(-90);
+        this.addButton(100, 580, 'downButton', () => this.moveShip('down')).setScale(0.1).setAngle(180); // Deixe a imagem de cabeça para baixo
+        this.addButton(150, 533, 'rightButton', () => this.moveShip('right')).setScale(0.1).setAngle(90);
+        this.addButton(700, 550, 'shootButton', () => this.shootBullet()).setScale(0.1); // Botão para atirar
+
+        // Adicione eventos para manter o movimento enquanto os botões estão pressionados
+        this.input.on('pointerdown', (pointer, currentlyOver) => {
+            currentlyOver.forEach(button => {
+                if (button.texture.key === 'upButton') this.isUpPressed = true;
+                if (button.texture.key === 'leftButton') this.isLeftPressed = true;
+                if (button.texture.key === 'downButton') this.isDownPressed = true;
+                if (button.texture.key === 'rightButton') this.isRightPressed = true;
+                if (button.texture.key === 'shootButton') this.isShootPressed = true;
+            });
+        });
+
+        this.input.on('pointerup', (pointer, currentlyOver) => {
+            currentlyOver.forEach(button => {
+                if (button.texture.key === 'upButton') this.isUpPressed = false;
+                if (button.texture.key === 'leftButton') this.isLeftPressed = false;
+                if (button.texture.key === 'downButton') this.isDownPressed = false;
+                if (button.texture.key === 'rightButton') this.isRightPressed = false;
+                if (button.texture.key === 'shootButton') this.isShootPressed = false;
+            });
+        });
     }
 
     update() {
         this.ship.setVelocity(0);
         
-        if (this.keyW.isDown && !this.keyS.isDown && !this.keyA.isDown && !this.keyD.isDown) {
+        if (this.keyW.isDown || this.isUpPressed) {
             this.ship.setVelocityY(-200);
             this.ship.setAngle(0); // Rotaciona a nave para cima
-        } else if (this.keyS.isDown && !this.keyW.isDown && !this.keyA.isDown && !this.keyD.isDown) {
+        } else if (this.keyS.isDown || this.isDownPressed) {
             this.ship.setVelocityY(200);
             this.ship.setAngle(180); // Rotaciona a nave para baixo
-        } else if (this.keyA.isDown && !this.keyW.isDown && !this.keyS.isDown && !this.keyD.isDown) {
+        } else if (this.keyA.isDown || this.isLeftPressed) {
             this.ship.setVelocityX(-200);
             this.ship.setAngle(-90); // Rotaciona a nave para a esquerda
-        } else if (this.keyD.isDown && !this.keyW.isDown && !this.keyS.isDown && !this.keyA.isDown) {
+        } else if (this.keyD.isDown || this.isRightPressed) {
             this.ship.setVelocityX(200);
             this.ship.setAngle(90); // Rotaciona a nave para a direita
         }
 
-        if (Phaser.Input.Keyboard.JustDown(this.spaceKey)) {
-            console.log(`Nave está no ângulo: ${this.ship.angle}`);
+        if (Phaser.Input.Keyboard.JustDown(this.spaceKey) || this.isShootPressed) {
             this.shootBullet();
+        }
+    }
+
+    addButton(x, y, key, callback) {
+        const button = this.add.sprite(x, y, key).setInteractive();
+        button.on('pointerdown', callback);
+        return button;
+    }
+
+    moveShip(direction) {
+        switch (direction) {
+            case 'up':
+                this.ship.setVelocityY(-200);
+                this.ship.setAngle(0); // Rotaciona a nave para cima
+                break;
+            case 'left':
+                this.ship.setVelocityX(-200);
+                this.ship.setAngle(-90); // Rotaciona a nave para a esquerda
+                break;
+            case 'down':
+                this.ship.setVelocityY(200);
+                this.ship.setAngle(180); // Rotaciona a nave para baixo
+                break;
+            case 'right':
+                this.ship.setVelocityX(200);
+                this.ship.setAngle(90); // Rotaciona a nave para a direita
+                break;
         }
     }
 
@@ -88,7 +147,7 @@ class MainScene extends Phaser.Scene {
                     bullet.setVelocityY(-300); // Bala para cima
                     bullet.setPosition(this.ship.x, this.ship.y - 20); // Ajusta a posição da bala para cima
                     break;
-                case 180:
+                case -180:
                     bullet.setVelocityY(300); // Bala para baixo
                     bullet.setPosition(this.ship.x, this.ship.y + 20); // Ajusta a posição da bala para baixo
                     break;
